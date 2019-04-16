e264843280e06c0b9d10988edb8505f3d40dd91d
1766241489@qq.com <1766241489@qq.com>
2019-04-16 22:36:54 +0800

    :sparkles: 增加首页的头部和底部

--- vue-cms/src/App.vue
+++ vue-cms/src/App.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
+    <div class="app-container">
 
+    <!-- 顶部 Header区域 -->
+<mt-header fixed title="我的vue项目"></mt-header>
+    <!-- 中间的路由 router-view区域 -->
+
+    <!-- 底部Tabber区域 -->
+    <!-- 这里使用MUI的底部样式 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+    <h1>123123</h1>
     </div>
 
 </template>
@@ -12,5 +37,9 @@
 
 <style lang="scss" scoped>
 
+.app-container{
+    padding-top: 40px;
+
+}
 </style>
 
--- vue-cms/src/index.js
+++ vue-cms/src/index.js
@@ -1,1 +1,19 @@
 console.log("ok")
+
+// import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+import './lib/mui/css/mui.css'
+
+import { Header  } from 'mint-ui'
+
+Vue.component(Header.name, Header )
+
+import Vue from 'vue'
+
+import app from './App.vue'
+
+var vm = new Vue({
+    el: '#app',
+    render: c=>c(app)
+})
